aafe7a2ce7e76102358c52e46dae6c6c55dc12c7
Bhavana <nbhavana999@gmail.com>
2021-03-15 10:16:00 -0500

    added readme

--- src/Components/LocationAndData.test.js
+++ src/Components/LocationAndData.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import LocationAndData from "../Components/LocationAndData";
 import "../cssFiles/LocationAndData.scss";
 
 it("renders without crashing", () => {
